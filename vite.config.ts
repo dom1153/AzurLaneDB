989323c09c35af33c8dd7cf3d98376a7ae236951
Dominic Dang <dtdd1153@live.com>
2023-07-26 16:02:27 -0500

    update base url for vite deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/AzurLaneDb/",
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
